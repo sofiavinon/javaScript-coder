86ada358d0b520a97d43bb9f4574895507f966d8
sofia aviñon <sofiaavignon@gmail.com>
2021-09-14 14:55:47 -0300

    Primera entrega

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+let numero1 = Number ( prompt("Ingrese un número") );
+let numero2 = Number ( prompt("Ingrese otro número") );
+
+ 
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+alert("El resultado de la suma es" + " " + suma + "," + " " + "de la resta es" + " " + resta + "," + " " + "de la multiplicación es" + " " + multiplicacion + "," + " " + "y de la división es" + " " + division );
